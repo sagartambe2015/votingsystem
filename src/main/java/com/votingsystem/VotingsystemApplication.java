f7a93f2ba983bc6d8b6090c69a0c05bc469f4b27
SAGAR <SAGAR@SAGAR-PC>
2018-01-14 17:32:18 +0530

    #Remove files #added entity

--- src/main/java/com/votingsystem/VotingsystemApplication.java
+++ src/main/java/com/votingsystem/VotingsystemApplication.java
@@ -1,4 +1,4 @@
-package com.votingsystem.votingsystem;
+package com.votingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/votingsystem/VotingsystemApplicationTests.java
+++ src/test/java/com/votingsystem/VotingsystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.votingsystem.votingsystem;
+package com.votingsystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
