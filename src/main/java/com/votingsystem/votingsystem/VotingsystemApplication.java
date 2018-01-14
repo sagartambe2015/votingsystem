8afcf84c28ccd3eaa72335d4752b6361cc724552
sagar sudhir tambe <sagarsudhirtambe@sagars-MacBook-Air.local>
2018-01-14 14:53:07 +0530

    Initial Commit

---
+++ src/main/java/com/votingsystem/votingsystem/VotingsystemApplication.java
@@ -0,0 +1,12 @@
+package com.votingsystem.votingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingsystemApplication.class, args);
+	}
+}
